7478a1664f87c1cf0927fb7aaca887df8f1f1778
Laura Angélica <lacardenasv@unal.edu.co>
2018-05-27 14:33:56 -0500

    animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,38 @@
 console.log('\'Allo \'Allo!');
 
 $(function() {
-  var cssSelector = anime({
-    targets: '#cssSelector .el',
-    translateX: 250
-  });
+
+  var baseTimeLine = anime.timeline();
+
+  baseTimeLine
+    .add({
+      targets: '.js-main-panel .js-main-panel-title',
+      translateY: 75,
+      duration: 3000,
+    })
+    .add({
+      targets: '.js-main-panel .js-main-panel-title',
+      opacity: 1,
+      offset: '-=2700'
+    })
+    .add({
+      targets: '.js-info-panel',
+      translateX: {
+        value: - $('.js-info-panel').width(),
+        duration: 3000
+      },
+      opacity: {
+        value: 1,
+        duration: 2500,
+        delay: 300
+      },
+      offset: '0'
+    })
+  /*var cssSelector = anime({
+    targets: '.js-main-panel .js-main-panel-title',
+    translateY: 25,
+    duration: 3000,
+    opacity: 1
+  });  */
 })
+
