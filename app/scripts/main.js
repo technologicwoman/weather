488da36e33f2bd219f262f8e9528a9ac11e1e7bb
Laura Angélica <lacardenasv@unal.edu.co>
2018-05-27 21:34:56 -0500

    animtions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,31 +8,57 @@ $(function() {
     .add({
       targets: '.js-main-panel .js-main-panel-title',
       translateY: 75,
-      duration: 3000,
-    })
-    .add({
-      targets: '.js-main-panel .js-main-panel-title',
-      opacity: 1,
-      offset: '-=2700'
+      opacity: {
+        value: 1,
+        delay: 550,
+        duration: 200
+      },
+      easing: 'easeInSine'
     })
     .add({
       targets: '.js-info-panel',
       translateX: {
-        value: - $('.js-info-panel').width(),
-        duration: 3000
+        value: - $('.js-info-panel').outerWidth(),
       },
       opacity: {
         value: 1,
-        duration: 2500,
         delay: 300
       },
-      offset: '0'
+      offset: '0',
+      easing: 'easeInSine'
     })
-  /*var cssSelector = anime({
-    targets: '.js-main-panel .js-main-panel-title',
-    translateY: 25,
-    duration: 3000,
-    opacity: 1
-  });  */
+    
+  var contentTimeLine = anime.timeline();
+  contentTimeLine
+    .add({
+      targets: '.js-main-panel .js-main-panel-image',
+      height: 175,
+      width: 175,
+      delay: '1400',
+      easing: 'easeOutExpo'
+    })
+    .add({
+      targets: '.js-main-panel-subtitle',
+      translateY: 10,
+      opacity: {
+        value: 1,
+      },
+      offset: '1500',
+      easing: 'easeInSine'
+    })
+    .add({
+      targets: '.js-main-panel-tmp',
+      translateX: {
+        value: '120px',
+        easing: 'easeInSine'
+      },
+      opacity: {
+        value: '1',
+        delay: 700,
+        easing: 'easeOutBack'
+      },
+      offset: '1500',
+    })
+
 })
 
