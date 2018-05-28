bc913861fe433bf3bed2be05c187eb2da62222bc
Laura Angélica <lacardenasv@unal.edu.co>
2018-05-28 12:12:55 -0500

    final commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,24 +22,74 @@ const weekDays = {
   6: 'Saturday',
 }
 
+const weatherMessage = {
+  'Clear': 'should enjoy your day outdoors!',
+  'Rain': 'If you go out, you should take an umbrella or stay in home',
+  'Clouds': 'should wear a coat to keep warm'
+}
+
+const predictions = {
+  3: {},
+  6: {},
+  9: {}
+}
+
 
 $(function() {
   let today = new Date();
   let currentDay = weekDays[today.getDay()];
   
-  $.get( "http://35.190.138.158:5002/predictions?city=asd&steps=2", function( data ) {
-    console.log(data);
+  $.get( "http://35.190.138.158:5002/predictions?city=asd&steps=3", function( data ) {
+    storeData(data.predictions);
     putCurrentData(data.current, data.temp, currentDay);
     initAnimation();
   });
   
+  $('.js-submit').on('click', showPrediction);
+
+  $('.js-close-modal').on('click', () => {
+    $('.js-prediction-modal').removeClass('is-active');
+    location.reload();
+  });
 
 })
 
-const putCurrentData = (currentWeather, currentTemp, currentDay) => {
-  let wtherImg;
-  let weather = currentWeather.toString();
+const showPrediction = () => {
+  let step = $('.js-step-select').val();
+  let name = $('.js-name').val();
+  let predictionData = predictions[step];
 
+  let max = {
+    value: 0
+  }
+
+  for(let property in predictionData) {
+    if (predictionData[property] > max.value){
+      max = {
+        value: predictionData[property],
+        weather: property
+      }
+    }
+  }
+
+  let weatherImg = chooseWeatherImg(max.weather);
+  let propability = max.value.toFixed(2);
+  let message = `${name} ${weatherMessage[max.weather]}, the weather would be ${max.weather} with a probability of ${propability}`;
+  $('.js-prediction-title').text(`Prediction to ${step} hours`);
+  $('.js-prediction-image').css("background-image" , `url(${weatherImg})`);
+  $('.js-prediction-description').text(message);
+  $('.js-prediction-modal').addClass('is-active');
+  predictionAnimation();
+}
+
+const storeData = (predictionsArray) => {
+  predictions[3] = predictionsArray[0];
+  predictions[6] = predictionsArray[1];
+  predictions[9] = predictionsArray[2];
+}
+
+const chooseWeatherImg = (weather) => {
+  let wtherImg;
   switch (weather) {
     case weathers.CLEAR:
       wtherImg = weatherImg.CLEAR
@@ -51,10 +101,63 @@ const putCurrentData = (currentWeather, currentTemp, currentDay) => {
       wtherImg = weatherImg.CLOUDS
       break;
   }
+  return wtherImg;
+}
+
+const putCurrentData = (currentWeather, currentTemp, currentDay) => {
+  let weather = currentWeather.toString();
+  let wtherImg = chooseWeatherImg(weather);
+
   $('.js-main-panel-title').text(currentDay);
   $('.js-number').text(currentTemp);
   $('.js-main-panel-image').css("background-image" , `url(${wtherImg})`);
 }
+
+const predictionAnimation = () => {
+  var contentTimeLine = anime.timeline();
+    contentTimeLine
+    .add({
+      targets: '.js-prediction-title',
+      translateY: 75,
+      opacity: {
+        value: 1,
+        delay: 550,
+        duration: 200
+      },
+      easing: 'easeInSine'
+    })
+    .add({
+      targets: '.js-main-panel .js-prediction-image',
+      height: 175,
+      width: 175,
+      delay: 500,
+      easing: 'easeOutExpo'
+    })
+    .add({
+      targets: '.js-prediction-subtitle',
+      translateY: 10,
+      opacity: {
+        value: 1,
+      },
+      offset: '1100',
+      easing: 'easeInSine'
+    })
+    .add({
+      targets: '.js-prediction-description',
+      translateX: {
+        value: '120px',
+        easing: 'easeInSine',
+        delay: 700,
+      },
+      opacity: {
+        value: '1',
+        delay: 1400,
+        easing: 'easeOutBack'
+      },
+      offset: '800',
+    })
+}
+
 
 const initAnimation = () => {
   var baseTimeLine = anime.timeline();
